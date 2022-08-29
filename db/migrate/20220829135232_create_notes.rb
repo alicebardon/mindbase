71d5b558b838478bc653bbf8d2a24581af0e3478
Jon Rhome <ffries610@comcast.net>
2022-08-29 15:53:37 +0200

    adding notes model

---
+++ db/migrate/20220829135232_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notes do |t|
+      t.string :content
+      t.string :file_path
+      t.string :language
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
