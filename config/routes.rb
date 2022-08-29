7227ba8b9c26a1c88215881b7a0e4ec0f56ea9a4
Maya Eyal <mayalindae@gmail.com>
2022-08-29 17:49:47 +0200

    added routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
+  get 'notes/new'
+  get 'notes/create'
+  get 'notes/edit'
+  get 'notes/update'
+  get 'notes/destroy'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :categories do
+    resources :notes, except: %i[show index]
+  end
 end
