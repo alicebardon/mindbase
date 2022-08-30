f1ec7a0ff42ddce8c2255e75ec5bd0affb0b2ff5
Maya Eyal <mayalindae@gmail.com>
2022-08-30 12:18:40 +0200

    started parsing file

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  JS_COMMENT = %r/^(.*)\/{2}\s*\$(.*)/
+  RUBY_COMMENT = %r/^(.*)#\s*\$(.*)/
   def new
   end
 
@@ -12,5 +14,9 @@ class NotesController < ApplicationController
   end
 
   def destroy
+  end
+
+  def parse_source_code(text)
+
   end
 end
