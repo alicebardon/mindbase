e8355dd2e0d2edda26c6f786c4696190077e7103
Maya Eyal <mayalindae@gmail.com>
2022-08-30 17:24:12 +0200

    removed conflicts from notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,5 @@
 class NotesController < ApplicationController
-<<<<<<< HEAD
-  JS_COMMENT = %r/^(.*)\/{2}\s*\$(.*)/
-  RUBY_COMMENT = /^(.*)#\s*\$(.*)/
-
-=======
   before_action :set_category, only: [:show, :edit, :update, :destroy]
->>>>>>> master
 
   def new
     @note = Note.new
@@ -28,12 +22,12 @@ class NotesController < ApplicationController
   end
 
   def update
-  @note = Note.find(params[:id])
-  if @note.update(note_params_edit)
-    redirect_to category_path(@category)
-  else
-    render :edit, status: :unprocessable_entity
-  end
+    @note = Note.find(params[:id])
+    if @note.update(note_params_edit)
+      redirect_to category_path(@category)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -43,35 +37,6 @@ class NotesController < ApplicationController
     redirect_to category_path(@category)
   end
 
-<<<<<<< HEAD
-  def parse_source_code
-    @sample_text = "// This is a javascript comment
-                for (let i = 0; i < 10; i++) { // $ This is a javascript comment for Mindbase
-                  console.log(array[i]); // $ I would like to upload this to Mindbase
-                # This is a ruby comment
-                array.each do |elem| # $ This is a ruby comment for Mindbase
-                # This is a python comment
-                <!-- This is an HTML comment -->
-                /* This is a CSS comment */"
-
-    @js_code = "// This is a javascript comment
-            for (let i = 0; i < 10; i++) { // $ This is a javascript comment for Mindbase
-            console.log(array[i]); // $ I would like to upload this to Mindbase
-            } // This is a comment I don't want to upload to Mindbase"
-    Note.destroy_all
-    user = User.first
-    matches = @js_code.scan(JS_COMMENT)
-    matches.each do |match_pair|
-      Note.create!(code_content: match_pair[0],
-                   content: match_pair[1],
-                   user: user,
-                   file_path: '/app/code.txt',
-                   language: 'javascript')
-    end
-    @notes = Note.all
-    @comment_char = "//"
-  end
-=======
   private
 
   def set_category
@@ -85,6 +50,4 @@ class NotesController < ApplicationController
   def note_params_edit
     params.require(:note).permit(:content, :code_content)
   end
-
->>>>>>> master
 end
