5dd41ee7f2a178e35c173344bcc1377740ec66ff
Jon Rhome <ffries610@comcast.net>
2022-09-01 17:56:02 +0200

    finished with the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "/categories"
+  root "categories#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
