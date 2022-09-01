2113bc28a8864e641de6374e8c8a8cf5d1c0c215
Alice Bardon <abardon.catineau@gmail.com>
2022-09-01 18:01:24 +0200

    popup upload file

---
+++ app/javascript/upload.js
@@ -0,0 +1,38 @@
+const initUploadPopup = () => {
+
+const openPopupButtons = document.querySelectorAll('[data-popup-target]')
+const overlay = document.getElementById('overlay')
+
+openPopupButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    const popup = document.querySelector(button.dataset.popupTarget)
+    openPopup(popup)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const popups = document.querySelectorAll('.popup.active')
+  popups.forEach(popup => {
+    closePopup(popup)
+  })
+})
+
+}
+
+
+function openPopup(popup) {
+  if (popup == null) return
+  popup.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+  if (popup == null) return
+  popup.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+export {
+  initUploadPopup
+}
