c821822907356f34d316ee0242daa8b9ddafc0dd
Maya Eyal <mayalindae@gmail.com>
2022-09-02 11:31:37 +0200

    deleted old file upload route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  post '/categories/upload_file', to: 'categories#upload_file', as: 'upload'
   resources :categories do
     resources :notes, except: %i[new create show index]
   end
