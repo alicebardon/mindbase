93b06fd7c78390340a34dd18af57fa0afb8f4f5f
Jon Rhome <ffries610@comcast.net>
2022-09-06 02:00:49 +0200

    adds appearing form

--- app/javascript/controllers/note_controller.js
+++ app/javascript/controllers/note_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="note"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "showme", "hideme"]
 
   connect() {
+    console.log("note controller connected")
+  }
+
+  new_note() {
+    console.log("new note triggered");
+    this.hidemeTarget.hidden = true;
+    this.showmeTarget.hidden = false;
   }
 
   send(event) {
-
     event.preventDefault()
 
     fetch(this.formTarget.action, {
