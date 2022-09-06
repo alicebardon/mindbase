6e37b4d6f5a1e877cf118cf5cdaf048ff8848dd4
Maya Eyal <mayalindae@gmail.com>
2022-09-06 13:18:58 +0200

    got rid of vanilla js code to make flashes disappear

--- app/javascript/controllers/flashes_controller.js
+++ app/javascript/controllers/flashes_controller.js
@@ -3,14 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flashes"
 export default class extends Controller {
   connect() {
-    console.log("I am the FLASHES controller!");
     setTimeout(() => {
-      console.log("deleting...")
       this.element.classList.add("alert-hidden");
+      setTimeout(() => {
+        this.element.remove();
+      }, 600);
     } , 3000)
-  }
-
-  fade_flash() {
-    console.log("I will make the flash fade away");
   }
 }
--- app/javascript/flashes.js
+++
@@ -1,9 +0,0 @@
-const makeFlashesDisappear = () => {
-  console.log("time our set")
-    setTimeout(() => {
-      console.log("time out called")
-      document.querySelector('.alert').style.display = 'none';
-    }, 3000);
-};
-
-export { makeFlashesDisappear };
