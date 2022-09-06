ff775e89e860f282c3fd96cb9b85b269cd465674
Jon Rhome <ffries610@comcast.net>
2022-09-06 19:11:26 +0200

    showing some data in the view

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,7 +1,10 @@
 class GithubController < ApplicationController
   def new
+    @user = current_user
+    @client = Octokit::Client.new(access_token: current_user.access_token)
   end
 
   def create
+    @client.repo_all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ Rails.application.routes.draw do
   get 'github/new'
   get 'github/create'
   get 'test/init'
-  # devise_for :users
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "categories#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   post '/categories/upload_file', to: 'categories#upload_file', as: 'upload'
-  resources :test
   resources :categories do
     resources :notes, except: %i[new create show index]
   end
   resources :notes, only: %i[new create]
+  resources :github, only: %i[new create]
 end
