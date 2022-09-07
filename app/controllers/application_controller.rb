29bb8abe38bb9fb8e241bd9db2b9c40fca498354
Jon Rhome <ffries610@comcast.net>
2022-09-08 01:38:52 +0200

    fs video on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   # Devise setup
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # rescue github auth errors
+  rescue_from StandardError do |exception|
+    e = exception
+    puts e.message
+    raise e
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
@@ -12,4 +19,6 @@ class ApplicationController < ActionController::Base
 
   def home
   end
+
+
 end
