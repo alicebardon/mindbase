be3c129197fb7d7c4d1dd41d1e756c6ad46f2bd3
Alice Bardon <abardon.catineau@gmail.com>
2022-09-07 23:27:50 +0200

    pop up for desktop done

--- app/javascript/controllers/show_desktop_controller.js
+++ app/javascript/controllers/show_desktop_controller.js
@@ -2,15 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-desktop"
 export default class extends Controller {
-  static targets = ["desktop"]
+  static targets = ["desktop", "choose"]
 
   connect() {
     // console.log("hey");
     // console.log(this.desktop);
+    console.log(this.choose);
   }
 
   revealImportDesktop(event) {
     event.preventDefault();
+    this.chooseTarget.classList.add("d-none")
     this.desktopTarget.classList.remove("d-none")
+    // this.chooseTarget.classList.remove("d-none")
   }
 }
