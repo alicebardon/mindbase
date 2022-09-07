b991bdf04b558d6beac0d205192d0791da803673
Maya Eyal <mayalindae@gmail.com>
2022-09-07 13:36:04 +0200

    switched to editing comment and code in place

--- app/javascript/controllers/note_edit_controller.js
+++ app/javascript/controllers/note_edit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="note-edit"
 export default class extends Controller {
-  static targets = ["comment", "code", "showEditForm", "editNoteForm"]
+  static targets = ["comment", "code", "comment", "codeEditor", "commentEditor"]
 
   connect() {
     console.log("Note edit controller now connected");
@@ -12,14 +12,16 @@ export default class extends Controller {
     console.log("Editing comment");
     console.log(this.commentTarget.textContent);
     this.commentTarget.classList.add("d-none");
-    this.codeTarget.classList.add("d-none");
     this.showEditFormTarget.classList.remove("d-none");
-    console.log(this.showEditFormTarget.hidden);
   }
 
   edit_code() {
     console.log("Editing code");
+    this.codeTarget.classList.add("d-none");
     console.log(this.codeTarget)
   }
 
+  update_code(event) {
+    console.log("Code updated!");
+  }
 }
