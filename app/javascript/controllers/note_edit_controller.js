3a5d9c834143b5f3fb0233dea8c3649cf561afee
Maya Eyal <mayalindae@gmail.com>
2022-09-08 13:58:02 +0200

    discarded text fields/areas in favor of contenteditable. much better

--- app/javascript/controllers/note_edit_controller.js
+++ app/javascript/controllers/note_edit_controller.js
@@ -3,33 +3,21 @@ import hljs from "highlight.js/lib/common"
 
 // Connects to data-controller="note-edit"
 export default class extends Controller {
-  static targets = ["comment", "code", "comment", "codeEditor", "commentEditor"]
+  static targets = ["comment", "code", "comment", "commentEditor"]
 
   connect() {
     console.log("Note edit controller now connected");
   }
 
-  edit_comment() {
-    console.log("Editing comment");
-    this.commentEditorTarget.classList.remove("d-none");
-    this.commentTarget.classList.add("d-none");
-  }
-
-  edit_code() {
-    console.log("Editing code");
-    this.codeEditorTarget.classList.remove("d-none");
-    this.codeTarget.classList.add("d-none");
-  }
-
   update_comment(event) {
-    const note_id = this.commentEditorTarget.dataset.noteId;
-    const request_body = JSON.stringify({ note: { comment: this.commentEditorTarget.value } });
+    const note_id = this.commentTarget.dataset.noteId;
+    const request_body = JSON.stringify({ note: { comment: this.commentTarget.innerText } });
     this.#update_request(note_id, request_body);
   }
 
   update_code(event) {
-    const note_id = this.codeEditorTarget.dataset.noteId;
-    const request_body = JSON.stringify({ note: { code: this.codeEditorTarget.value } });
+    const note_id = this.codeTarget.dataset.noteId;
+    const request_body = JSON.stringify({ note: { code: this.codeTarget.innerText.trim() } });
     this.#update_request(note_id, request_body);
   }
 
