b33a47dd7260df4dd3e40a1f1f6b47d364af6a56
Maya Eyal <mayalindae@gmail.com>
2022-09-08 17:18:08 +0200

    fixed bug in adding new note through GUI

--- app/javascript/controllers/note_controller.js
+++ app/javascript/controllers/note_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import { end } from "@popperjs/core";
 import hljs from "highlight.js/lib/common"
 
 // Connects to data-controller="note"
 export default class extends Controller {
-  static targets = ["form", "showme", "hideme"]
+  static targets = ["form", "newNote", "bigButton",
+                    "comment", "code", "language"]
 
   connect() {
     console.log("note controller connected")
@@ -12,25 +14,38 @@ export default class extends Controller {
 
   new_note() {
     console.log("new note triggered");
-    this.hidemeTarget.hidden = true;
-    this.showmeTarget.hidden = false;
+    this.bigButtonTarget.hidden = true;
+    this.newNoteTarget.hidden = false;
   }
 
   send(event) {
     event.preventDefault();
+    console.log(this.commentTarget.value);
+    console.log(this.codeTarget.value);
+    console.log(this.languageTarget.value);
+    const comment = this.commentTarget.value;
+    const code = this.codeTarget.value;
+    const language = this.languageTarget.value;
+
+    if (code == "" || comment == "" || language == "") {
+      return;
+    }
 
     fetch(this.formTarget.action, {
       method: "POST",
       headers: { "Accept": "application/json" },
       body: new FormData(this.formTarget)
     })
-      .then(response => response.json())
+      .then(response => {
+        return response.json();
+      })
       .then((data) => {
+        console.log(data)
         this.formTarget.outerHTML = data.form;
         this.element.insertAdjacentHTML('beforebegin', data.inserted_item);
         hljs.highlightAll();
-        this.showmeTarget.hidden = false;
-        this.hidemeTarget.hidden = true;
+        this.newNoteTarget.hidden = true;
+        this.bigButtonTarget.hidden = false;
         console.log("SENT!");
       });
   }
