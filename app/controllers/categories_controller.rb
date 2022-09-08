52c0481899fa2b1b079c6bad4ff165f290e0a242
Alice Bardon <abardon.catineau@gmail.com>
2022-09-08 10:06:56 +0200

    moving branch to gh popup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,10 @@ class CategoriesController < ApplicationController
     else
       @categories = current_user.categories
     end
+
+    @client = Octokit::Client.new(access_token: current_user.access_token)
+    @repos = @client.repos
+
   end
 
 
