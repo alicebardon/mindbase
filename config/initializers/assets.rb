d929e83ae3d503cbaa8362700f8711e59284df39
Jon Rhome <ffries610@comcast.net>
2022-09-08 09:49:58 +0200

    making progress on the home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Precompile additional assets.
